70154f93649691c41fcca3314e78ea6ffb28df30
github username <github email address>
2020-06-04 08:24:07 -0700

    cleaned up more code

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -18,3 +18,89 @@ weatherInit = () => {
        });
      }
  }
+
+
+ // geoFindMe(){
+ //
+ //   function success(position){
+ //      const latitude  = position.coords.latitude;
+ //      const longitude = position.coords.longitude;
+ //      const newAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=2ebf31cbd8d1d525b4f75bedc92f60f0`
+ //      console.log(latitude)
+ //    }
+ //
+ //   navigator.geolocation.getCurrentPosition(success)
+ //
+ // }
+
+
+
+
+ //
+ //   getData(api){
+ //   fetch(api)
+ //   .then(response => response.json())
+ //   .then(data => {
+ //         this.setState({
+ //           city: data.name,
+ //           temperature: data.main.temp,
+ //           description: data.weather[0].description
+ //         })
+ //       });
+ //       return this.state
+ // }
+ //
+
+
+
+
+   // componentDidMount(){
+   //
+   //   function success(position) {
+   //     const latitude  = position.coords.latitude;
+   //     const longitude = position.coords.longitude;
+   //
+   //     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=2ebf31cbd8d1d525b4f75bedc92f60f0`)
+   //     .then(response => response.json())
+   //     .then(data => {
+   //           this.setState({
+   //             city: data.name,
+   //             temperature: data.main.temp,
+   //             description: data.weather[0].description
+   //           })
+   //         });
+   //   }
+   //     navigator.geolocation.getCurrentPosition(success)
+   // }
+
+
+   // geoFindMe() {
+   //
+   //   function success(position) {
+   //     const latitude  = position.coords.latitude;
+   //     const longitude = position.coords.longitude;
+   //
+   //     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=2ebf31cbd8d1d525b4f75bedc92f60f0`)
+   //     .then(response => response.json())
+   //     .then(data => {
+   //       this.setState({
+   //         city: data.name,
+   //         temperature: data.main.temp,
+   //         description: data.weather[0].description
+   //       })
+   //     });
+   //   }
+   //     navigator.geolocation.getCurrentPosition(success)
+   // }
+
+   // geoFindMe() {
+   //
+   //   function success(position) {
+   //     const latitude  = position.coords.latitude;
+   //     const longitude = position.coords.longitude;
+   //
+   //
+   //     };
+   //   }
+   //     navigator.geolocation.getCurrentPosition(success)
+   // }
